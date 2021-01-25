04f96425bf181acebb220e02c689ee769c61bd8f
ThejaYamjala <thejayamjala123@gmail.com>
2021-01-25 00:14:23 -0500

    Add notes on Java and data types and the HelloIDE example

---
+++ HelloIDE/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Spam and Egggs"); //here's where I print some text
+		// Different ways to make comments
+		//1.this is the first way
+		/* i can write comments like this
+		 so i can write in between the / * and * /
+		 */
+		/**
+		 * this is a javadoc comment, Typically used to comment on methods and provide documentation
+		 */
+	}
+
+}
